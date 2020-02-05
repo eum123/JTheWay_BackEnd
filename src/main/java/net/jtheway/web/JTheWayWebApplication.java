f8a7bb03bf1d7fd7bc0469d754d041fa492bc68b
eummanjin <eummanjin@gmail.com>
2020-02-05 04:53:28 +0000

    init

--- src/main/java/net/jtheway/web/JTheWayWebApplication.java
+++ src/main/java/net/jtheway/web/JTheWayWebApplication.java
@@ -1,5 +1,16 @@
 package net.jtheway.web;
 
-public class JTheWayWebApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
+@SpringBootApplication
+public class JTheWayWebApplication {
+	public static void main(String ...args) {
+		SpringApplication.run(JTheWayWebApplication.class, args);
+	}
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JTheWayWebApplication.class);
+	}
 }
